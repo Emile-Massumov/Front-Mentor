324f4566b8ee6f4349451057a32ae2a6e35e0d71
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-04-09 22:29:36 -0500

    added JS

---
+++ main.js
@@ -0,0 +1,26 @@
+const btns= document.querySelectorAll('[role="circle"]');
+const submitBtn= document.getElementById('submit');
+const thankYouBox= document.getElementById('thank-you-box');
+const selectedRating= document.getElementById('selected-rating');
+const feedBackBox= document.getElementById('feedback-box');
+
+btns.forEach((btn) => {
+    btn.addEventLisner('click', function(e) {
+    let selectedBtn= e.currentTarget;
+    for (let i=0 ; i<btns.length; i++) {
+
+        if (btns[i].getAttribute('aria-selected')=='true') {
+            btns[i].setAttribute('aria-selected', false);
+            selectedBtn.setAttribute('aria-selected', true);
+            selectedRating.textContent=selectedBtn.id;
+        }else{ 
+            selectedBtn.setAttribute('aria-selected', true);
+            selectedRating.textContent=selectedBtn.id;
+        }
+    }
+})
+})
+submitBtn.addEventListener('click', function() {
+    feedBackBox.setAttribute('hidden', true);
+    thankYouBox.removeAttribute('hidden');
+})
